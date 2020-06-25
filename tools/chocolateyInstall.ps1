928cbd30fb841f09b8486f729c266cbea8481423
Marc-André Moreau <mamoreau@devolutions.net>
2020-06-25 09:46:56 -0400

    choco-wayk-now: update to 2020.2.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,18 +1,18 @@
 ﻿$ErrorActionPreference = 'Stop';
-$packageName= 'Wayk Now'
-$Version = "2019.2.1.0"
+$PackageName = 'Wayk Now'
+$Version = "2020.2.0.0"
 
 $packageArgs = @{
-  packageName   = $packageName
+  packageName   = $PackageName
   fileType      = 'MSI'
-  url           = "https://cdn.devolutions.net/download/Wayk/2019.2.1.0/WaykNow-x86-2019.2.1.0.msi#_ga=2.10116147.725310804.1573225798-1075188698.1557089024"
-  url64bit      = "https://cdn.devolutions.net/download/Wayk/2019.2.1.0/WaykNow-x64-2019.2.1.0.msi#_ga=2.10116147.725310804.1573225798-1075188698.1557089024"
+  url           = "https://cdn.devolutions.net/download/Wayk/${Version}/WaykNow-x86-${Version}.msi"
+  url64bit      = "https://cdn.devolutions.net/download/Wayk/${Version}/WaykNow-x64-${Version}.msi"
   softwareName  = "Wayk Now*"
-  checksum      = '7B63C6240E0731313E2C3931EF14E475BFCD28F243A349ABFDD78BEA3731C867'
+  checksum      = 'C5703E35920E265ACAB3E2D3FE9709D05EBC15F589873B06DA121EF9DEF90EE5'
   checksumType  = 'sha256'
-  checksum64    = '2FA20998218FFA397A4705AA70B121EAB89A7DBEF804D79FC7FAE2F1C7AE3CD4'
+  checksum64    = 'E03E987D51735E685324F48FBD89984ABBD34591BF8FF0ECB24A1E1EDD311B8B'
   checksumType64= 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($PackageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
   validExitCodes= @(0, 3010, 1641)
   }
 
